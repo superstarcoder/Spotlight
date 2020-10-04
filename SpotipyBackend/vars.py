1da5b79cf2b6b967548356638dffc563f25b39f0
superstarcoder <dhanishnatarajan@gmail.com>
2020-10-04 00:07:49 -0700

    made some bug fixes to genres. genres sorting now works :D. also fixed some suggestion bugs too because fun

--- SpotipyBackend/vars.py
+++ SpotipyBackend/vars.py
@@ -9,4 +9,5 @@ userid: {"username":username, "searchResults": [data], "chosenSong":songName}
 }
 """
 suggestForm = {}
-genres = {"pop": "Pop", "pop-rap": "Pop Rap", "lo-fi": "Lo-fi", "metal": "Metal", "world": "World"}
+#genres = {"pop": "Pop", "pop-rap": "Pop Rap", "lo-fi": "Lo-fi", "metal": "Metal", "world": "World"}
+genres = {"pop": "pop", "pop-rap": "pop-rap", "lo-fi": "lo-fi", "metal": "metal", "world": "world"}
